5d6e5757bfdca44a9a0534687174d153543318ab
Arthur Ghazaryan <newartroll11@gmail.com>
2021-09-14 23:59:40 +0400

    feat:(material ui)

---
+++ src/components/myButton/MyButton.js
@@ -0,0 +1,9 @@
+import styles from './MyButton.module.css'
+
+export default function MyButton() {
+    return (
+        <div className={styles.btn}>
+          <p>Book a call</p>
+        </div>
+    )
+}
