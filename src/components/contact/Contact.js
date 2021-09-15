b8b56b2da94d5b6c4e1673ca4393f100b62669cb
Arthur <newartroll11@gmail.com>
2021-09-15 16:11:17 +0400

    feat:(axios and states adding)

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,8 +2,41 @@ import LogoPart from "../logoPart/LogoPart";
 import styles from "./Contact.module.css";
 import mail from "../../imgs/Message.png";
 import img from "../../imgs/shuttle.png";
+import { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhote] = useState("");
+  const [message, setMessage] = useState("");
+  const [isValid, setIsValid] = useState({
+    nameValue: "",
+    emailValue: "",
+    phoneValue: "",
+    messageValue: "",
+  });
+
+  function checkName(e) {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
+
+  function checkEmail(e) {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  }
+
+  function checkPhone(e) {
+    console.log(e.target.value);
+    setPhote(e.target.value);
+  }
+
+  function checkMessage(e) {
+    console.log(e.target.value);
+    setMessage(e.target.value);
+  }
+
+  function checking() {}
   return (
     <div className={styles.contact}>
       <div className={styles.left}>
@@ -25,20 +58,35 @@ const Contact = () => {
         <div className={styles.lChild}>
           <div className={styles.forinps}>
             <p>Name</p>
-            <input type="text" placeholder="Name" />
+            <input
+              value={name}
+              onChange={checkName}
+              type="text"
+              placeholder="Name"
+            />
           </div>
           <div className={styles.forinps}>
             <p>Email</p>
-            <input type="text" placeholder="name@email.com" />
+            <input
+              value={email}
+              onChange={checkEmail}
+              type="text"
+              placeholder="name@email.com"
+            />
           </div>
           <div className={styles.forinps}>
             <p>Phone</p>
-            <input type="text" placeholder="+123 568 6354" />
+            <input
+              value={phone}
+              onChange={checkPhone}
+              type="text"
+              placeholder="+123 568 6354"
+            />
           </div>
         </div>
         <div className={styles.forMessage}>
           <p>Message</p>
-          <input type="text" />
+          <input value={message} onChange={checkMessage} type="text" />
         </div>
         <div></div>
         <div className={styles.compBtn}>
