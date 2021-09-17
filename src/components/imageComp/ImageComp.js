1dc7dd89e559cdb2e692450accd2c572f82874de
Arthur <newartroll11@gmail.com>
2021-09-17 11:59:32 +0400

    feat:(adaptive intpart)

--- src/components/imageComp/ImageComp.js
+++ src/components/imageComp/ImageComp.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './ImageComp.module.css'
+import React from "react";
+import styles from "./ImageComp.module.css";
 
 const ImageComp = (props) => {
-    return (
-        <div className={styles.imgPart}>
-            <img alt='img' src={props.img} />
-        </div>
-    );
-}
+  return (
+    <div className={styles.imgPart}>
+      <img alt="img" src={props.img} />
+    </div>
+  );
+};
 
 export default ImageComp;
